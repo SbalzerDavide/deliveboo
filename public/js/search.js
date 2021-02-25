a8bc7f8acff03140c4e2df69c2bd3b8a0f268a87
davide sbalzer <sbalzerdavide@hotmail.it>
2021-02-25 15:49:22 +0100

    clean filterjs

--- public/js/search.js
+++ public/js/search.js
@@ -31760,11 +31760,7 @@ var search = new vue__WEBPACK_IMPORTED_MODULE_0___default.a({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 module.exports = __webpack_require__(/*! /Users/davidesbalzer/Documents/buffoni/informatica/atom/deliveroo/DeliveBoo/DeliveBoo/resources/js/search.js */"./resources/js/search.js");
-=======
-module.exports = __webpack_require__(/*! C:\Users\Acer\Desktop\DeliveBoo\resources\js\search.js */"./resources/js/search.js");
->>>>>>> 0f2e7f558ac7d7b9c44a3fb7d8b19d78783aa786
 
 
 /***/ })
