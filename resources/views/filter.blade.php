92e66240b88e0e6d944bbb760b02c0f66b5460a4
FrancescoGalanti <galanti_francesco>
2021-03-02 11:51:33 +0100

    working genres

--- resources/views/filter.blade.php
+++ resources/views/filter.blade.php
@@ -2,34 +2,37 @@
 
 @section('content')
 <div>
-    <div class="container ">
-        <h1>ADVANCED RESEARCH</h1>
-        <h3>You are inside "{{ $name }}" category</h3>
-        <span class="genre"></span>
-        <h3>search restaurant by name</h3>
-        <input v-model="searchText" @keyup.enter="makeSearch">
-        <button @click="makeSearch">
-            Search
-        </button>
-        
-        {{-- <div class="click" @click="takeGenre">take</div> --}}
-        <div class="container-restaurant-genres">
-            <div v-if="listRestaurant.length>0" class="restaurant-container">
+    <div class="hero-rest">
+      <div class="container">
+        <div class="box">
+          <h3>ADVANCED RESEARCH <br> You are inside genre {{$name}}</h3>
+          <input class="input-primary" v-model="searchText"  placeholder="search restaurant by name" @keyup.enter="makeSearch">
+          <div class="btn-list large" @click="makeSearch">
+             <i class="fas fa-arrow-right"></i>
+          </div>
+        </div>
+      </div> 
+    </div>
+    <div class="container">
+      <div class="container-restaurant-genres">
+         <div v-if="listRestaurant.length>0" class="restaurant-container">
                 <ul >
                     <a id="no-decoration" v-for="element in listRestaurant" :href="element.route">
-                        <li >
-                            <img :src="'http://127.0.0.1:8000/storage/' + element.path_image" width="100%" alt="">
+                        <li class="rest-box">
+                            <img class="zoom-in" :src="'http://127.0.0.1:8000/storage/' + element.path_image" width="100%" alt="">
                             <div class="rest-text">
                                 <h5>@{{element.name}}</h5> 
                             </div>
                         </li>
                     </a>
                 </ul>
-            </div>
-            <h5 v-else>there are no results</h5>
-        </div>
+                <div class="btn-list mt-5 mb-5" @click="moreRestaurants">
+                    show more results
+                </div>
+          </div>
+          <h5 v-else>there are no results</h5>
+      </div>
     </div>
-
 </div>
 
 
